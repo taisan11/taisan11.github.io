9230cf1645799b68232f723efe3854ccdc2e4108
taisan11 <131413638+taisan11@users.noreply.github.com>
2023-06-02 12:10:24 +0900

    aaa

--- ousamagame/main.js
+++ ousamagame/main.js
@@ -3,4 +3,5 @@ function nameset() {
     const name2 = "プレイヤー2";
     const name3 = "プレイヤー3";
     document.getElementById("nameok").innerHTML="名前一覧" + "\nプレイヤー1:" + name1 + "\nプレイヤー2:" + name2 + "\3プレイヤー3:" + name3;
+    parent.removeChild(document.getElementById("b1"))
 };
