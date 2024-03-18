1fbf00c5d512967449ec5800f449a1aa104a5417
taisan <131413638+taisan11@users.noreply.github.com>
2024-03-18 19:48:44 +0900

    a

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,6 +4,9 @@ import Mlink from '../component/mlink'
 // import type { Meta } from '../types'
 
 export default createRoute((c) => {
+  const posts = import.meta.glob<{ frontmatter: Meta }>('./blog/*.mdx', {
+    eager: true,
+  })
   return c.render(
     <div class="tyuusinn">
       <h1>taisanサイト</h1>
@@ -14,6 +17,13 @@ export default createRoute((c) => {
         <Mlink to="about" title="自己紹介" date="2024-2-19"/><br />
         <a href="http://" id='dona'>寄付!!(予告)</a>
         <h3>めにゅー(記事)</h3>
+        {Object.entries(posts).map(([id, module]) => {
+          if (module.frontmatter) {
+            return (
+              <Mlink to={`${id.replace(/\.mdx$/, '')}`} title={module.frontmatter.title} date={`${id.replace(/\.mdx$/, '')}`}/>
+            )
+          }
+        })}
         <Mlink to="blog/2024-2-13" title="本日日記#1" date="2024-2-13"/>
       </div>
       <p>©taisan11</p>
